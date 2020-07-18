88aaae5326bb622ad47f39a085842616eb6b0d5a
Gabryjiel <33352685+Gabryjiel@users.noreply.github.com>
2020-07-18 19:21:22 +0200

    BE & package.json: Small impovements

--- express/db/index.js
+++ express/db/index.js
@@ -3,21 +3,22 @@ const { Client } = require('pg')
 require('dotenv').config()
 
 let config = {}
-if(process.env.NODE_ENV == 'development')
-  config = {
-      'user': process.env.DB_USER,
-      'password': process.env.DB_PASSWORD,
-      'host': process.env.DB_HOST,
-      'database': process.env.DB_DATABASE,
-      'port': process.env.DB_PORT
-  }
-else if(process.env.NODE_ENV == 'production')
+if(process.env.NODE_ENV == 'production')
     config = {
-      'connectionString': process.env.DB_URI,
+      'connectionString': process.env.DATABASE_URL,
       'ssl': {
         rejectUnauthorized: false
       }
   }
+else{
+  config = {
+    'user': process.env.DB_USER,
+    'password': process.env.DB_PASSWORD,
+    'host': process.env.DB_HOST,
+    'database': process.env.DB_DATABASE,
+    'port': process.env.DB_PORT
+  }
+}
 
 const client = new Client(config)
 client.connect();
